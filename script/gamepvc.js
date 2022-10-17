8176c457827a59b65a822f507b548b8bb321cbee
huguestouyon <hugues.touyon@gmail.com>
2022-10-17 22:59:52 +0200

    send

--- script/gamepvc.js
+++ script/gamepvc.js
@@ -1,21 +1,29 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     const cases = Array.from(document.querySelectorAll('.case'))
+    let roundNb = 0
 
-    let board = [
-        ['','',''],
-        ['','',''],
-        ['','','']
-    ]
-    const player = 'X'
-    const computer = 'O'
-    let win = false
-    let turn = 0
-
-    function game() {
-        if(turn % 2 === 0) {
-         //function pour récupérer les coordonnées via le onclick
-         // board(cord)
+    function tour() {
+        roundNb++
+        if(roundNb % 2 === 0) {
+            return 'O'
+        }
+        else {
+            return 'X'
         }
     }
-    game()
+
+    function win() {
+        if(cases[0].innerHTML !== '' && cases[0].innerHTML === cases[1].innerHTML && cases[1].innerHTML === cases[2].innerHTML) {
+            console.log('Ok')
+        }
+    }
+
+    function jouer() {
+        if(!this.innerHTML) {
+            this.innerHTML = tour()
+        }
+        win()
+    }
+
+    cases.forEach(element => element.addEventListener('click', jouer))
 })
